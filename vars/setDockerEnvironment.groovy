89bbae2e8b4549d57bc2e2edd89e2620beb369fd
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-03-01 19:22:00 -0500

    Continue...

--- vars/setDockerEnvironment.groovy
+++ vars/setDockerEnvironment.groovy
@@ -16,10 +16,12 @@ def call(DockerHost dockerHost, Closure body) {
 }
 
 def withCredentials(DockerHost dockerHost, String credentialId, Closure body) {
-  withCredentials([
-    usernamePassword(credentialsId: credentialId, usernameVariable: 'DOCKER_CREDENTIALS_USR', passwordVariable: 'DOCKER_CREDENTIALS_PSW')
-  ]) {
-    sh 'echo "$DOCKER_CREDENTIALS_PSW" | docker login --username "$DOCKER_CREDENTIALS_USR" --password-stdin'
-    setDockerEnvironment(dockerHost, body)
+  setDockerEnvironment(dockerHost) {
+    withCredentials([
+      usernamePassword(credentialsId: credentialId, usernameVariable: 'DOCKER_CREDENTIALS_USR', passwordVariable: 'DOCKER_CREDENTIALS_PSW')
+    ]) {
+      sh 'echo "$DOCKER_CREDENTIALS_PSW" | docker login --username "$DOCKER_CREDENTIALS_USR" --password-stdin'
+      body()
+    }
   }
 }
