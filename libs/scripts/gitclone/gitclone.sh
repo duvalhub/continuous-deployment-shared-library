83a755ba8ecface9d2b134bf34465400389d81a3
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-03-12 20:27:20 -0400

    Continue...

--- libs/scripts/gitclone/gitclone.sh
+++ libs/scripts/gitclone/gitclone.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "### Running in gitClone.sh"
 echo "### GIT_URL: '$GIT_URL', GIT_DIRECTORY: '$GIT_DIRECTORY', GIT_SSH_COMMAND: '$GIT_SSH_COMMAND'"
-rm -rf $GIT_DIRECTORY
+rm -rf "$GIT_DIRECTORY"
 ls -l "$SSH_KEY_PATH"
+cat "$SSH_KEY_PATH"
 #export GIT_SSH_COMMAND="ssh -oStrictHostKeyChecking=accept-new -i $SSH_KEY_PATH  -F /dev/null" 
 git --version
-git clone $GIT_URL $GIT_DIRECTORY
+git clone "$GIT_URL" "$GIT_DIRECTORY"
