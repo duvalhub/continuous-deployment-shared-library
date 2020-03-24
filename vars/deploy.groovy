3f670a28734320668dbb4e6de0b6dac98e4ce82b
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-03-23 20:30:02 -0400

    Continue...

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,16 +1,14 @@
 import com.duvalhub.deploy.DeployRequest
-import com.duvalhub.writecompose.WriteComposeRequest
 
 def call(DeployRequest request) {
-  stage('Deploy') {
-    echo "### Deploying an artifact. DeployRequest: '${request.toString()}'"
-    String composeFilePath = writeCompose(request)
-    setDockerEnvironment.withCredentials(request.getDockerHost(request.environment), request.getCredentialId()) {
-      String prepare_script = "${env.PIPELINE_WORKDIR}/libs/deploy/scripts/network/createIfNotExist.sh"
-      String params = "--network ${request.getInternalNetwork()}"
-      executeScript(prepare_script, false, params)
-      sh "docker stack deploy --with-registry-auth -c ${composeFilePath} ${request.getStackName()}"
+    stage('Deploy') {
+        echo "### Deploying an artifact. DeployRequest: '${request.toString()}'"
+        String composeFilePath = writeCompose(request)
+        setDockerEnvironment.withCredentials(request.getDockerHost(request.environment), request.getCredentialId()) {
+            String prepare_script = "${env.PIPELINE_WORKDIR}/libs/deploy/scripts/network/createIfNotExist.sh"
+            String params = "--network ${request.getInternalNetwork()}"
+            executeScript(prepare_script, false, params)
+            sh "docker stack deploy --with-registry-auth -c ${composeFilePath} ${request.getStackName()}"
+        }
     }
-  }
 }
-
