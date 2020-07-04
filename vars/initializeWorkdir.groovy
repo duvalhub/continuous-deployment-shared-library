bc1f213d38376f04ae4b0f124b1f8fbd39c17817
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-07-04 00:41:38 -0400

    Continue...

--- vars/initializeWorkdir.groovy
+++ vars/initializeWorkdir.groovy
@@ -80,7 +80,7 @@ def downloadConfigFile(String branch, GitRepo gitRepo) {
         )
     }
  */
-    def response = httpRequest authentication: 'SERVICE_ACCOUNT_GITHUB_TOKEN_TODELETE', url: configUrl
+    def response = httpRequest authentication: 'SERVICE_ACCOUNT_GITHUB_TOKEN_TODELETE', url: configUrl, outputFile: 'config.yml', validResponseCodes: "200,404"
 
 //    return httpRequest authentication: 'SERVICE_ACCOUNT_GITHUB_TOKEN_TODELETE', url: configUrl, outputFile: 'config.yml', validResponseCodes: "200,404"
 
