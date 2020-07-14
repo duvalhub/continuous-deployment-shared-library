b14d8c522f2dc7a5a5582d087bb28711d41dbf1f
Maxthod <47720660+Maxthod@users.noreply.github.com>
2020-07-14 00:16:47 -0400

    fix/append-stackname-on-networks-and-volumes-name-when-internal (#15)

--- libs/deploy/scripts/processYml/processYmlTest.sh
+++ libs/deploy/scripts/processYml/processYmlTest.sh
@@ -4,16 +4,20 @@ export STACK_NAME="caramba"
 export APP_NAME="app_name"
 export IMAGE="africa"
 export NETWORKS="totoafrica banana isExternal;external"
-export VOLUMES="isExternal:/src/etc avol2:/src/oekp"
+export VOLUMES="isExternal:/src/etc;external avol2:/src/oekp"
 export HOSTS="totoafrica.com"
-export ENV_VARIABLES="TOTO=pitou
+export ENV_VARIABLES="TOTO=pitou"
+
+echo "####################"
+echo "############ Processing"
+echo "####################"
+
+temp_file=$(mktemp)
+./processYml.sh "$temp_file"
 
 echo "####################"
 echo "############ Result"
 echo "####################"
-
-temp_file=$(mktemp)
-./processYml.sh "$temp_file" > /dev/null
 cat "$temp_file"
 rm -rf "$temp_file"
 
