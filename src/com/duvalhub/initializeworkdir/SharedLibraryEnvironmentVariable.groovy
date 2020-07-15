d02ec3d86abe9b7c9f1ea4ba9d886e2e18b74670
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-07-15 00:27:53 -0400

    Continue...

---
+++ src/com/duvalhub/initializeworkdir/SharedLibraryEnvironmentVariable.groovy
@@ -0,0 +1,12 @@
+package com.duvalhub.initializeworkdir
+
+enum SharedLibraryEnvironmentVariable {
+
+    SHARED_LIBRARY_ENVIRONMENT_VARIABLE("SHARED_LIBRARY_VERSION");
+
+    String name;
+
+    SharedLibraryEnvironmentVariable(String name) {
+        this.name = name;
+    }
+}
---
+++ src/com/duvalhub/initializeworkdir/SharedLibraryVersion.java
@@ -0,0 +1,12 @@
+package com.duvalhub.initializeworkdir;
+
+import java.util.Map;
+
+public class SharedLibraryVersion {
+    public static void set(Map<String,String> env, String value) {
+        env.put(SharedLibraryEnvironmentVariable.SHARED_LIBRARY_ENVIRONMENT_VARIABLE.name(), value);
+    }
+    public static String get(Map<String,String> env) {
+       return env.get(SharedLibraryEnvironmentVariable.SHARED_LIBRARY_ENVIRONMENT_VARIABLE.name());
+    }
+}
