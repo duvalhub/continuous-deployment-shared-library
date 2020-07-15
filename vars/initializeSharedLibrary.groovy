80032eade2046bdf0ba64c75cdbc72f52ad6a3ae
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-07-15 00:02:19 -0400

    Continue...

--- vars/initializeSharedLibrary.groovy
+++ vars/initializeSharedLibrary.groovy
@@ -5,8 +5,7 @@ def call(InitializeWorkdirIn params = new InitializeWorkdirIn()) {
     env.PIPELINE_WORKDIR = "$WORKSPACE/${params.pipelineWorkdir}"
     echo "### PIPELINE_WORKDIR variable set to '${env.PIPELINE_WORKDIR}'"
     def pipelineBranch = env.PIPELINE_BRANCH ?: "master"
-    //def pipelineBranch = sh(script: "env | grep 'library.shared-library.version' | cut -d '=' -f 2", returnStdout: true, àà).trim()
-    sh "rm -rf $PIPELINE_WORKDIR && git clone ${params.pipelineGitRepo.getHttpUrl()} -b ${pipelineBranch} $PIPELINE_WORKDIR"
+    sh "rm -rf $PIPELINE_WORKDIR && git clone '${params.pipelineGitRepo.getHttpUrl()}' -b '${pipelineBranch}' $PIPELINE_WORKDIR"
 }
 
 def stage(InitializeWorkdirIn params = new InitializeWorkdirIn()) {
@@ -19,6 +18,7 @@ def findSharedLibraryVersion(){
     node('master') {
         def branch = sh(script: "env | grep 'library.shared-library.version' | cut -d '=' -f 2", returnStdout: true).trim()
         env.PIPELINE_BRANCH = branch
+        echo "Setting 'env.PIPELINE_BRANCH' from shared-library version. Found: '$branch'"
         return branch
     }
 }
