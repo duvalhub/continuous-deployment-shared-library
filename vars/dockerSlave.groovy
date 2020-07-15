dcf5088312966ab10ecf5f0ff85db10ff28c201d
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-07-15 00:08:04 -0400

    Continue...

--- vars/dockerSlave.groovy
+++ vars/dockerSlave.groovy
@@ -1,4 +1,9 @@
 def call(Closure body) {
+
+    if(!env.PIPELINE_BRANCH) {
+        initializeSharedLibrary.findVersion()
+    }
+
     def dockerSlaveImage = 'duvalhub/jenkins-slave:1.0.4'
     dockerNode(dockerSlaveImage) { 
       body()
