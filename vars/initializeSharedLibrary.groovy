ffbfa20b4be57d93916669723aea79484fe5a3e0
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-07-14 23:57:49 -0400

    Continue...

--- vars/initializeSharedLibrary.groovy
+++ vars/initializeSharedLibrary.groovy
@@ -4,14 +4,21 @@ def call(InitializeWorkdirIn params = new InitializeWorkdirIn()) {
     echo "### Cloning Jenkins into Workdir..."
     env.PIPELINE_WORKDIR = "$WORKSPACE/${params.pipelineWorkdir}"
     echo "### PIPELINE_WORKDIR variable set to '${env.PIPELINE_WORKDIR}'"
-    def pipelineBranch = sh(script: "env | grep 'library.shared-library.version' | cut -d '=' -f 2", returnStdout: true).trim()
-    sh "env"
-    echo "Pipelinebarnch : '$pipelineBranch' "
+    def pipelineBranch = env.PIPELINE_BRANCH ?: "master"
+    //def pipelineBranch = sh(script: "env | grep 'library.shared-library.version' | cut -d '=' -f 2", returnStdout: true, àà).trim()
     sh "rm -rf $PIPELINE_WORKDIR && git clone ${params.pipelineGitRepo.getHttpUrl()} -b ${pipelineBranch} $PIPELINE_WORKDIR"
 }
 
 def stage(InitializeWorkdirIn params = new InitializeWorkdirIn()) {
     stage("Initialization Shared Library") {
         call(params)
+    }
+}
+
+def findSharedLibraryVersion(){
+    node('master') {
+        def branch = sh(script: "env | grep 'library.shared-library.version' | cut -d '=' -f 2", returnStdout: true).trim()
+        env.PIPELINE_BRANCH = branch
+        return branch
     }
 }
