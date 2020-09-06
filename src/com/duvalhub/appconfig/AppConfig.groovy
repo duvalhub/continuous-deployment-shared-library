65bfcce60fa973527c85a6f43464f037c4811ba0
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-09-06 18:29:31 -0400

    Continue...

--- src/com/duvalhub/appconfig/AppConfig.groovy
+++ src/com/duvalhub/appconfig/AppConfig.groovy
@@ -63,7 +63,7 @@ class Network {
 }
 class Docker {
     String registry = "docker.io"
-    String registryApi = "registry.docker.com/v1"
+    String registry_api = "registry.hub.docker.com/v1"
     String namespace
     String repository
     String credentialId
