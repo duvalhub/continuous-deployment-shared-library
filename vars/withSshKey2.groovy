96e4343cdf23ae19a7fc29a211866f917638277b
Maxthod <maxthod@pm.me>
2021-04-05 21:44:24 -0400

    Continue...

--- vars/withSshKey2.groovy
+++ vars/withSshKey2.groovy
@@ -1,5 +1,5 @@
 def call(String host, String credentialId, Closure body) {
-    echo "### Setting SSH Config File for $host using $credentialId..."
+    echo "### Setting SSH Config File for ${host} using ${credentialId}..."
     String keyFileVar = "SSH_KEY_PATH"
     withCredentials([
             sshUserPrivateKey(keyFileVariable: keyFileVar, credentialsId: credentialId)
@@ -12,6 +12,7 @@ def call(String host, String credentialId, Closure body) {
                 "HOST=${host}",
                 "CRED_VAR=${keyFileVar}"
         ]) {
+            echo 'SSH_HOME=$SSH_HOME, SSH_CONFIG=$SSH_CONFIG, HOST=$HOST, CRED_VAR=$CRED_VAR'
             sh 'mkdir -p $SSH_HOME'
             sh 'echo "Host $HOST" > $SSH_CONFIG'
             sh 'echo "HostName $HOST" >> $SSH_CONFIG'
