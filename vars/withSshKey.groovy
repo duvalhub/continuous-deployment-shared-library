99f506da059430042b239e9aa446efdb0886ff0d
Maxthod <maxthod@pm.me>
2021-04-05 21:08:06 -0400

    Continue...

--- vars/withSshKey.groovy
+++ vars/withSshKey.groovy
@@ -3,9 +3,9 @@ def call (Closure body) {
     withCredentials([
         sshUserPrivateKey(keyFileVariable: 'SSH_KEY_PATH', credentialsId: "SERVICE_ACCOUNT_SSH")
     ]) {
-        String ssh_key_path = env.SSH_KEY_PATH
+        String ssh_var_name = "SSH_KEY_PATH"
         withEnv([
-            "GIT_SSH_COMMAND=ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -i ${ssh_key_path} -F /dev/null"
+            "GIT_SSH_COMMAND=ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -i ${ssh_var_name} -F /dev/null"
         ]) {
             sh "git config --global user.email \"toto-africa@email.com\""
             sh "git config --global user.name \"Toto Africa\""
