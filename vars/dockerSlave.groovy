650b8d89cd7b5469549813ea1ee4b76190761fdc
Maxthod <maxthod@pm.me>
2021-04-06 18:34:26 -0400

    Continue...

--- vars/dockerSlave.groovy
+++ vars/dockerSlave.groovy
@@ -4,7 +4,7 @@ def call(Closure body) {
     if(!SharedLibrary.getVersion(env)) {
         initializeSharedLibrary.findVersion()
     }
-    String image = 'duvalhub/jenkins-slave:1.0.5'
+    String image = 'duvalhub/jenkins-slave:1.0.5.rc1'
     dockerNode(image) {
       body()
     }
