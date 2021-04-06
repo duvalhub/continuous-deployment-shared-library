d5a9519b0c861257d896a27f5214429fc6b79336
Maxthod <maxthod@pm.me>
2021-04-05 22:51:21 -0400

    Continue...

--- vars/withSshKey2.groovy
+++ vars/withSshKey2.groovy
@@ -1,27 +1,26 @@
-def call(String host, String credentialId, Closure body) {
+def call(String host, String credentialId, String user, Closure body) {
     echo "### Setting SSH Config File for ${host} using ${credentialId}..."
-    String keyFileVar = "SSH_KEY_PATH"
+    String credVar = "SSH_KEY_PATH"
     withCredentials([
-            sshUserPrivateKey(keyFileVariable: keyFileVar, credentialsId: credentialId)
+            sshUserPrivateKey(keyFileVariable: credVar, credentialsId: credentialId)
     ]) {
         String sshFolder = "/home/jenkins/.ssh"
         String sshConfig = "${sshFolder}/config"
         withEnv([
                 "SSH_HOME=${sshFolder}",
                 "SSH_CONFIG=${sshConfig}",
+                "SSH_USER=${user}",
                 "HOST=${host}",
-                "CRED_VAR=${keyFileVar}"
+                "CRED_VAR=${credVar}"
         ]) {
-            sh 'echo SSH_HOME=$SSH_HOME, SSH_CONFIG=$SSH_CONFIG, HOST=$HOST, CRED_VAR=$CRED_VAR'
             sh 'mkdir -p $SSH_HOME'
             sh 'echo "Host $HOST" > $SSH_CONFIG'
-            sh 'echo "User jenkins" > $SSH_CONFIG'
+            sh 'echo "User $SSH_USER" > $SSH_CONFIG'
             sh 'echo "HostName $HOST" >> $SSH_CONFIG'
             sh 'bash -c \'echo "IdentityFile ${!CRED_VAR}" >> $SSH_CONFIG\''
             sh 'echo "StrictHostKeyChecking=no" >> $SSH_CONFIG'
+            sh 'echo "UserKnownHostsFile=/dev/null" >> $SSH_CONFIG'
         }
-        sh "cat ${sshConfig}"
-        sh "ssh ${host} \"ls -l\""
         body()
     }
 }
