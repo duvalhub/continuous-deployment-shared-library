343351a99e9d7d4881587710285c0bd954b401bd
Maxthod <maxthod@pm.me>
2021-04-05 21:09:17 -0400

    Continue...

--- vars/withSshKey2.groovy
+++ vars/withSshKey2.groovy
@@ -1,14 +1,15 @@
 def call (Closure body) {
     echo "### Setting GIT_SSH_COMMAND environment variable"
+    String keyFileVar ="SSH_KEY_PATH"
     withCredentials([
-        sshUserPrivateKey(keyFileVariable: 'SSH_KEY_PATH', credentialsId: "SERVICE_ACCOUNT_SSH_2")
+        sshUserPrivateKey(keyFileVariable: keyFileVar, credentialsId: "SERVICE_ACCOUNT_SSH_2")
     ]) {
         String ssh_key_path = env.SSH_KEY_PATH
         String sshConfig = "/home/jenkins/.ssh/config"
         sh "mkdir -p /home/jenkins/.ssh"
         sh "echo \"Host *\" > ${sshConfig}"
         sh "echo \"HostName vps287088.duvalhub.com\" >> ${sshConfig}"
-        sh "echo \"IdentityFile ${env.SSH_KEY_PATH}\" >> ${sshConfig}"
+        sh "echo \"IdentityFile \$${keyFileVar}\" >> ${sshConfig}"
         sh "echo \"StrictHostKeyChecking=no\" >> ${sshConfig}"
 
 
