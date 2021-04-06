cd789641c9924b07ff738bac6797f77fdc6e28b8
Maxthod <maxthod@pm.me>
2021-04-05 20:55:10 -0400

    Continue...

--- vars/withSshKey2.groovy
+++ vars/withSshKey2.groovy
@@ -4,10 +4,15 @@ def call (Closure body) {
         sshUserPrivateKey(keyFileVariable: 'SSH_KEY_PATH', credentialsId: "SERVICE_ACCOUNT_SSH_2")
     ]) {
         String ssh_key_path = env.SSH_KEY_PATH
-        withEnv([
-            "GIT_SSH_COMMAND=ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -i ${ssh_key_path} -F /dev/null"
-        ]) {
-            body()
-        }
+        String sshConfig = "/home/jenkins/.ssh/config"
+        sh """
+            mkdir -p /home/jenkins/.ssh
+            echo "Host *" > ${sshConfig}
+            echo "HostName vps287088.duvalhub.com >> ${sshConfig}
+            echo "IdentityFile ${env.SSH_KEY_PATH} >> ${sshConfig}
+        """
+        sh "cat ${sshConfig}"
+        ssh 'ssh root@vps287088.duvalhub.com "ls -l"'
+        body()
     }
 }
