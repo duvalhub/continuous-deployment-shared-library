95184275d10e6b955b78c5c631c525362c8f5101
Maxthod <maxthod@pm.me>
2021-04-05 20:56:22 -0400

    Continue...

--- vars/withSshKey2.groovy
+++ vars/withSshKey2.groovy
@@ -6,10 +6,10 @@ def call (Closure body) {
         String ssh_key_path = env.SSH_KEY_PATH
         String sshConfig = "/home/jenkins/.ssh/config"
         sh """
-            mkdir -p /home/jenkins/.ssh
-            echo "Host *" > ${sshConfig}
-            echo "HostName vps287088.duvalhub.com >> ${sshConfig}
-            echo "IdentityFile ${env.SSH_KEY_PATH} >> ${sshConfig}
+            mkdir -p /home/jenkins/.ssh;
+            echo "Host *" > ${sshConfig}";
+            echo "HostName vps287088.duvalhub.com" >> ${sshConfig};
+            echo "IdentityFile ${env.SSH_KEY_PATH}" >> ${sshConfig};
         """
         sh "cat ${sshConfig}"
         ssh 'ssh root@vps287088.duvalhub.com "ls -l"'
