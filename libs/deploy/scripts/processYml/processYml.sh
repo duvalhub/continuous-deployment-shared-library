55c87fc0c6db5b9c6cd2e0082dae94cdb20e15d4
Maxthod <maxthod@pm.me>
2021-04-06 23:52:31 -0400

    Continue...

--- libs/deploy/scripts/processYml/processYml.sh
+++ libs/deploy/scripts/processYml/processYml.sh
@@ -95,6 +95,7 @@ if [ -n "$PORT" ]; then
     yq w -i "$TMP_YML" "$BASE_PATH.deploy.labels.\"reverseproxy.port\"" "$PORT"
 fi
 
+
 if [ -n "$HOSTS" ]; then
     yq w -i "$TMP_YML" "$BASE_PATH.deploy.labels.\"reverseproxy.host\"" "\"$HOSTS\""
     yq w -i "$TMP_YML" "$BASE_PATH.deploy.labels.\"reverseproxy.ssl\"" "\"true\""
