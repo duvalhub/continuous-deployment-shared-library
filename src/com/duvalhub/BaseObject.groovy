2f50ef65e533a75e0504e2cbd1c11a9f031a56ee
Maxthod <maxthod@pm.me>
2021-04-07 20:51:31 -0400

    Continue...

--- src/com/duvalhub/BaseObject.groovy
+++ src/com/duvalhub/BaseObject.groovy
@@ -3,8 +3,12 @@ package com.duvalhub
 import groovy.json.JsonBuilder
 
 
-class BaseObject  {
+class BaseObject extends Expando {
     String toString() {
         return new JsonBuilder(this).toPrettyString()
+    }
+
+    def propertyMissing(name, value) {
+        // nothing
     }
 }
