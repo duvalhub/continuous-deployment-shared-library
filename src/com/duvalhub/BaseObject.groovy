15018ef232893c87f50f0fd1039d024f4af91f3f
Maxthod <maxthod@pm.me>
2021-04-07 20:49:39 -0400

    Continue...

--- src/com/duvalhub/BaseObject.groovy
+++ src/com/duvalhub/BaseObject.groovy
@@ -2,14 +2,8 @@ package com.duvalhub
 
 import groovy.json.JsonBuilder
 
-trait IgnoreUnknownProperties {
-    def propertyMissing(String name, value) {
-        // do nothing
-//        echo "Missing property $name"
-    }
-}
 
-class BaseObject implements IgnoreUnknownProperties {
+class BaseObject  {
     String toString() {
         return new JsonBuilder(this).toPrettyString()
     }
