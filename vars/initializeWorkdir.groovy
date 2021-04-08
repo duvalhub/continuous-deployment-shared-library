4995785fd09bc424dfc67fa678d13d66a5c6f51f
Maxthod <maxthod@pm.me>
2021-04-07 22:34:46 -0400

    Continue...

--- vars/initializeWorkdir.groovy
+++ vars/initializeWorkdir.groovy
@@ -1,3 +1,4 @@
+import com.duvalhub.appconfig.AppConfig
 import com.duvalhub.git.GitCloneRequest
 import com.duvalhub.git.GitRepo
 import com.duvalhub.initializeworkdir.InitializeWorkdirIn
@@ -7,10 +8,14 @@ import com.duvalhub.initializeworkdir.SharedLibrary
 def call(InitializeWorkdirIn params = new InitializeWorkdirIn()) {
     echo "### Initializing Work Directory. InitializeWorkdirIn: '${params.toString()}'"
     def pipelineBranch = SharedLibrary.getVersion(env) ?: "master"
+    GitRepo appGitRepo = params.getAppGitRepo()
+
+    echo "### Getting app config file"
+    AppConfig appConfig = getConfigFile(pipelineBranch, appGitRepo)
+
+    return appConfig
 
     initializeSharedLibrary(params)
-
-    GitRepo appGitRepo = params.getAppGitRepo()
 
     if (!appGitRepo) {
         echo "### Lets find 'appGitRepo' from 'checkout scm'"
@@ -25,10 +30,8 @@ def call(InitializeWorkdirIn params = new InitializeWorkdirIn()) {
             appGitRepo = new GitRepo(org, repo, branch)
         }
     }
-    echo "### Getting app config file"
-    getConfigFile(pipelineBranch, appGitRepo)
 
-    if(params.getCloneAppRepo()) {
+    if (params.getCloneAppRepo()) {
         echo "### Cloning App into Workdir..."
         GitCloneRequest appRequest = new GitCloneRequest(appGitRepo, params.appWorkdir)
         gitClone(appRequest)
@@ -42,12 +45,42 @@ def stage(InitializeWorkdirIn params = new InitializeWorkdirIn()) {
     }
 }
 
-def getConfigFile(String branch, GitRepo gitRepo) {
-    def response = downloadConfigFile(branch, gitRepo);
+def getMergedFile(String branch, GitRepo gitRepo) {
+    String configFile = "config.yml"
+    getConfigFile(branch, gitRepo, configFile)
+    def configs = readYaml(file: configFile)
+    while (configs.parent) {
+        def parentFile = 'parent.yml'
+        def response = downloadConfigFile(configs.parent, parentFile)
+        configs.parent = null
+        if (response.status == 200) {
+            def parent = readYaml(file: configFile)
+            configs = merge(configs, parent)
+        }
+    }
+    return configs
+}
+
+def merge(Map lhs, Map rhs) {
+    return rhs.inject(lhs.clone()) { map, entry ->
+        if (map[entry.key] instanceof Map && entry.value instanceof Map) {
+            map[entry.key] = merge(map[entry.key], entry.value)
+        } else if (map[entry.key] instanceof Collection && entry.value instanceof Collection) {
+            map[entry.key] += entry.value
+        } else {
+            map[entry.key] = entry.value
+        }
+        return map
+    }
+}
+
+def getConfigFile(String branch, GitRepo gitRepo, String destination) {
+    String configUrl = String.format("https://raw.githubusercontent.com/duvalhub/continous-deployment-configs/%s/%s/%s/config.yml", branch, gitRepo.getOrg(), gitRepo.getRepo())
+    def response = downloadConfigFile(configUrl, destination);
     if (response.status == 404) {
         if (branch != 'master') {
             echo "Config file not found on branch '${branch}'. Trying branch 'master'"
-            response = downloadConfigFile('master', gitRepo)
+            return getConfigFile('master', gitRepo, destination)
         }
 
         if (response.status == 404) {
@@ -55,18 +88,16 @@ def getConfigFile(String branch, GitRepo gitRepo) {
             sh "exit 1"
         }
     }
-    echo "File downloaded and is supposely at config.yml"
-    return response
+    echo "File downloaded and is supposedly at ${destination}"
+    return branch
 }
 
-def downloadConfigFile(String branch, GitRepo gitRepo) {
-    def configUrl = String.format("https://raw.githubusercontent.com/duvalhub/continous-deployment-configs/%s/%s/%s/config.yml", branch, gitRepo.getOrg(), gitRepo.getRepo())
-    echo "Downloading the config file from url: '${configUrl}'"
-
+def downloadConfigFile(String configUrl, String destination) {
+    echo "Downloading the config file from url: '${configUrl}' to ${destination}"
     return httpRequest(
             authentication: 'SERVICE_ACCOUNT_GITHUB_TOKEN',
             url: configUrl,
-            outputFile: 'config.yml',
+            outputFile: destination,
             validResponseCodes: "200,404"
     )
 }
