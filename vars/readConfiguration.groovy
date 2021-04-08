f18f72e9961884be2915cc30d4ed724f2a6c174d
Maxthod <maxthod@pm.me>
2021-04-07 21:03:04 -0400

    Continue...

--- vars/readConfiguration.groovy
+++ vars/readConfiguration.groovy
@@ -4,12 +4,13 @@ import org.codehaus.groovy.runtime.metaclass.MissingPropertyExceptionNoStack
 def call() {
     AppConfig conf = valueOf(readYaml(file:'config.yml'))
     echo conf.toString()
+    sh "exit 1"
     return conf
 }
 
 AppConfig valueOf(Map<String, Object> source) {
     try {
-        return new AppConfig(source)
+        return new AppConfig()
     } catch (MissingPropertyExceptionNoStack e) {
         echo e.getMessage()
         source.remove(e.property)
