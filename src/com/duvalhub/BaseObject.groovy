d6cce1f8f57953e0ac17607752f386e2096c47ad
Maxthod <maxthod@pm.me>
2021-04-07 21:47:00 -0400

    Continue...

--- src/com/duvalhub/BaseObject.groovy
+++ src/com/duvalhub/BaseObject.groovy
@@ -1,12 +1,13 @@
 package com.duvalhub
 
+import com.cloudbees.groovy.cps.NonCPS
 import groovy.json.JsonBuilder
 
 class BaseObject {
     String toString() {
         return new JsonBuilder(this).toPrettyString()
     }
-
+    @NonCPS
     def propertyMissing(name, value) {
         // nothing
     }
