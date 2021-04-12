25fd33b4de8747e77e5e1d54e4f8f5cb55527b8c
Maxthod <maxthod@pm.me>
2021-04-12 00:54:59 -0400

    Continue...

--- libs/build/templates/wrappers/start.sh
+++ libs/build/templates/wrappers/start.sh
@@ -3,7 +3,9 @@
 declare env_files="${ENVIRONMENT_FILES:-""}"
 
 for file in $env_files; do
-  source <(sed 's/^/export /g' "$file")
+  mkfifo /tmp/source_fifo;
+  sed 's/^/export /g' "$file" > /tmp/source_fifo & source /tmp/source_fifo
+  rm -f /tmp/source_fifo
 done
 
 "$@"
