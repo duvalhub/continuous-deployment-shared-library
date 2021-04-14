560347bd9d7c055e0d7d4f32b36012a217e2473d
Maxthod <maxthod@pm.me>
2021-04-14 01:14:46 -0400

    Continue...

--- libs/build/templates/wrappers/start.sh
+++ libs/build/templates/wrappers/start.sh
@@ -4,5 +4,10 @@ for file in /run/secrets/*; do
   . "$file"
 done
 
-sleep infinity
+####################
+sleep 100000000000
+# Why do we not have /entrypoint.sh in our image ????
+# We are based on nginx:alpine therefore we should have it.
+####################
+
 exec "$@"
