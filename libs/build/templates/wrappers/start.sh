b439ac56c1de416ea982283917e726369de7a272
Maxthod <maxthod@pm.me>
2021-04-14 19:03:11 -0400

    Continue...

--- libs/build/templates/wrappers/start.sh
+++ libs/build/templates/wrappers/start.sh
@@ -3,14 +3,15 @@
 if [ -d /run/secrets/ ]; then
   tmp_file=$(mktemp)
   for file in /run/secrets/*; do
+    echo "Sourcing $file"
     sed 's/^/export /g' "$file" >"$tmp_file"
-    . "$tmp_file"
+    source "$tmp_file"
   done
   rm -f "$tmp_file"
 fi
-
+env
 ####################
-#sleep infinity
+sleep infinity
 # Why do we not have /entrypoint.sh in our image ????
 # We are based on nginx:alpine therefore we should have it.
 ####################
