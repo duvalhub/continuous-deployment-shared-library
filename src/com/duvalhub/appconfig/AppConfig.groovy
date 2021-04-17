a9addbed0f9c2b25823b604c9300d6822dc2e203
Maxthod <maxthod@pm.me>
2021-04-16 23:56:39 -0400

    "feature/database-by-configuration"

--- src/com/duvalhub/appconfig/AppConfig.groovy
+++ src/com/duvalhub/appconfig/AppConfig.groovy
@@ -8,11 +8,13 @@ class AppConfig extends BaseObject {
     Deploy deploy
     Docker docker
 }
+
 class App extends BaseObject {
     String name
     String group
     String version_control
 }
+
 class Build extends BaseObject {
     String builder
     String builder_version = "latest"
@@ -21,16 +23,31 @@ class Build extends BaseObject {
     String container_version = "alpine"
     DockerHost host
 }
+
 class Deploy extends BaseObject {
     String port = "80"
+    Database database
     Platforms platforms
 }
+
+class Database extends BaseObject {
+    boolean enabled = true
+    String secretId
+    String image = "mysql"
+    String version = "10.5"
+}
+
+enum Environment {
+    dev, stage, prod;
+}
+
 class Platforms extends BaseObject {
     Platform base
     Platform dev
     Platform stage
     Platform prod
 }
+
 class Platform extends BaseObject {
     String[] hostnames
     Boolean defaultHostname = true
@@ -42,6 +59,7 @@ class Platform extends BaseObject {
     DockerHost host
     int replicas = 1
 }
+
 class Volume extends BaseObject {
     String name
     String destination
@@ -50,17 +68,19 @@ class Volume extends BaseObject {
         return String.format("%s:%s", this.name, this.destination)
     }
 }
+
 class Network extends BaseObject {
     String name
     boolean external
 
     String toString() {
-        if(external) {
+        if (external) {
             return String.format("%s;external", this.name)
         }
         return this.name
     }
 }
+
 class Docker extends BaseObject {
     String registry = "docker.io"
     String registryApi = "https://registry.hub.docker.com/v1"
@@ -68,7 +88,7 @@ class Docker extends BaseObject {
     String repository
     String credentialId
 
-    String getDockerImage(){
+    String getDockerImage() {
         return String.format("%s/%s/%s", this.registry, this.namespace, this.repository)
     }
 }
--- src/com/duvalhub/appconfig/AppConfigAccessor.groovy
+++ src/com/duvalhub/appconfig/AppConfigAccessor.groovy
@@ -2,8 +2,10 @@ package com.duvalhub.appconfig
 
 import com.duvalhub.BaseObject
 
+import javax.xml.crypto.Data
+
 class AppConfigAccessor extends BaseObject {
-    String scriptPath = "libs/deploy/scripts/processYml/processYml.sh"
+    String scriptPath = "libs/deploy/scripts/writeCompose/writeCompose.sh"
     String compose = "docker-compose.yml"
 
     AppConfig appConfig
@@ -96,6 +98,10 @@ class AppConfigAccessor extends BaseObject {
         return this.appConfig.deploy.port
     }
 
+    Database getDatabase() {
+        return this.appConfig.deploy.database
+    }
+
     String getDomainNames(String environment) {
 
         Platform platform = this.getPlatform(environment)
@@ -159,8 +165,8 @@ class AppConfigAccessor extends BaseObject {
         Platform merged
         if (base) {
             merged = (base.properties.findAll { k, v -> v }  // p1's non-null properties
-                    + host.properties.findAll { k, v -> v }) // plus p2's non-null properties
-                    .findAll { k, v -> k != 'class' } as Platform
+            +host.properties.findAll { k, v -> v } ) // plus p2's non-null properties
+            . findAll { k, v -> k != 'class' } as Platform
         } else {
             merged = host
         }
