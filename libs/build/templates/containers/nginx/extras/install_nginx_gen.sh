5992a090eae772e2955ff23f51800db82d346a58
Maxthod <maxthod@pm.me>
2021-11-23 01:08:26 -0500

    Continue...

--- libs/build/templates/containers/nginx/extras/install_nginx_gen.sh
+++ libs/build/templates/containers/nginx/extras/install_nginx_gen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y wget
+#apt-get update
+#apt-get install -y wget
 wget https://github.com/duvalhub/nginx-file-replace/raw/feature/initial/bin/nginx-gen
 chmod +x nginx-gen
