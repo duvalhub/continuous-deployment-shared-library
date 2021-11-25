cd923adf70b59723fc654103272f13218e7a8961
Maxthod <maxthod@pm.me>
2021-11-24 23:23:30 -0500

    Continue...

--- libs/build/templates/containers/nginx/extras/install_nginx_gen.sh
+++ libs/build/templates/containers/nginx/extras/install_nginx_gen.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 
+
 NGINX_GEN_VERSION=feature/initial
 wget "https://github.com/duvalhub/nginx-file-replace/raw/$NGINX_GEN_VERSION/nginx.tmpl"
 wget "https://github.com/duvalhub/nginx-file-replace/raw/$NGINX_GEN_VERSION/bin/nginx-gen"
