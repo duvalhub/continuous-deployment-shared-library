095fd1774f18055ba1742bf2cae517f709ec3a56
Maxthod <maxthod@pm.me>
2021-11-24 23:40:55 -0500

    Continue...

--- libs/build/templates/containers/nginx/extras/install_nginx_gen.sh
+++ libs/build/templates/containers/nginx/extras/install_nginx_gen.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
-
-
-
-NGINX_GEN_VERSION=feature/initial
-wget "https://github.com/duvalhub/nginx-file-replace/raw/$NGINX_GEN_VERSION/nginx.tmpl"
-wget "https://github.com/duvalhub/nginx-file-replace/raw/$NGINX_GEN_VERSION/bin/nginx-gen"
+NGINX_GEN_NAME="nginx-file-replace"
+NGINX_GEN_VERSION=1.0.0
+wget "https://github.com/duvalhub/nginx-file-replace/archive/refs/tags/$NGINX_GEN_VERSION.tar.gz"
+tar -xzf "$NGINX_GEN_VERSION.tar.gz"
+mv "$NGINX_GEN_NAME-$NGINX_GEN_VERSION/nginx.tmpl" ./
+mv "$NGINX_GEN_NAME-$NGINX_GEN_VERSION/bin/nginx-gen ./
 chmod +x nginx-gen
