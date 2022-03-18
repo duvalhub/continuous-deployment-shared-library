76aa2356e383283d9434412b16068b3549420f4d
Maxthod <maxthod@pm.me>
2022-03-17 23:57:20 -0400

    improvement/database-container-placement-contraints

--- libs/deploy/scripts/writeCompose/writeCompose.sh
+++ libs/deploy/scripts/writeCompose/writeCompose.sh
@@ -124,6 +124,7 @@ fi
 if [ -n "$DATABASE_IMAGE" ] && [ -n "$DATABASE_VERSION" ]; then
   # Create Database Service
   yq w -i "$TMP_YML" "services.database.image" "${DATABASE_IMAGE}:${DATABASE_VERSION}"
+  yq w -i "$TMP_YML" "services.database.deploy.placement.constraints[+]" "node.labels.database==true"
 
   # Database secrets (database name, username, password)
   add_thing_to_service "secrets" "$DATABASE_SECRET" "services.database"
