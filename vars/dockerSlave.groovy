28b4e06ce6ff099cb440dd1123b3a68be1c2bffa
Maxthod <maxthod@pm.me>
2022-03-25 00:30:26 -0400

    "fix/launching-container-during-build"

--- vars/dockerSlave.groovy
+++ vars/dockerSlave.groovy
@@ -5,7 +5,7 @@ def call(Closure body) {
         initializeSharedLibrary.findVersion()
     }
     String image = 'duvalhub/jenkins-slave:1.0.5.rc1'
-    dockerNode(image) {
+    dockerNode(image, '-v /var/jenkins:/home/jenkins') {
       body()
     }
 }
