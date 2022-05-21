1fc8c8c83fe527b7ac854e8a01b7af1d56977ef4
Maxthod <maxthod@pm.me>
2022-05-21 13:36:24 -0400

    Continue...

--- vars/setDockerEnvironment.groovy
+++ vars/setDockerEnvironment.groovy
@@ -5,10 +5,10 @@ def call(DockerHost dockerHost, Closure body) {
     String host = dockerHost.getUrl()
     String user = dockerHost.getUser()
     withSshKey(host, "SERVICE_ACCOUNT_SSH", "jenkins") {
-        sh """
 //            docker context inspect ${host} &>/dev/null || \
 //            docker context create ${host} --description 'Context for ${host}' --docker 'host=ssh://${user}@${host}'
 //            docker context use ${host}
+        sh """
             docker context use default
         """
         body()
