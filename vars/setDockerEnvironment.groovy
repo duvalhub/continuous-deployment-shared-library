459090339d18dc163c46ea1dcf88ed2b9717ac8e
Maxthod <maxthod@pm.me>
2022-05-21 18:08:27 -0400

    Continue...

--- vars/setDockerEnvironment.groovy
+++ vars/setDockerEnvironment.groovy
@@ -1,36 +1,34 @@
 import com.duvalhub.appconfig.DockerHost
 
 def call(DockerHost dockerHost, Closure body) {
-    body()
-//    return
-//    echo "Setting docker environment using SSH. dockerHost: '${dockerHost.toString()}'"
-//    String host = dockerHost.getUrl()
-//    String user = dockerHost.getUser()
-//    sh """
-//        docker context rm -f ${host}
-//        docker context create ${host} --description 'Context for ${host}' --docker 'host=ssh://${user}@${host}'
-//        docker context use ${host}
-//        docker ps
-//    """
-//
-//    withSshKey(host, "SERVICE _ACCOUNT_SSH", user) {
-//        sh """
-//            docker context rm -f ${host}
-//            docker context create ${host} --description 'Context for ${host}' --docker 'host=ssh://${user}@${host}'
-//            docker context use ${host}
-//            docker ps
-//        """
-//        body()
-//    }
+    echo "Setting docker environment using SSH. dockerHost: '${dockerHost.toString()}'"
+    String host = dockerHost.getUrl()
+    String user = dockerHost.getUser()
+    withSshKey(host, "SERVICE _ACCOUNT_SSH", user) {
+        sh """
+            docker context rm -f ${host}
+            docker context create ${host} --description 'Context for ${host}' --docker 'host=ssh://${user}@${host}'
+            docker context use ${host}
+            docker ps
+        """
+        body()
+    }
 }
+
+def login(String credentialId, Closure body) {
+    echo "Login into Docker Registry. credentialId: '${credentialId}'"
+    withCredentials([
+            usernamePassword(credentialsId: credentialId, usernameVariable: 'DOCKER_CREDENTIALS_USR', passwordVariable: 'DOCKER_CREDENTIALS_PSW')
+    ]) {
+        sh 'echo "$DOCKER_CREDENTIALS_PSW" | docker login --username "$DOCKER_CREDENTIALS_USR" --password-stdin'
+        body()
+    }
+}
+
 
 def withCredentials(DockerHost dockerHost, String credentialId, Closure body) {
     setDockerEnvironment(dockerHost) {
-        echo "Login into Docker Registry. credentialId: '${credentialId}'"
-        withCredentials([
-                usernamePassword(credentialsId: credentialId, usernameVariable: 'DOCKER_CREDENTIALS_USR', passwordVariable: 'DOCKER_CREDENTIALS_PSW')
-        ]) {
-            sh 'echo "$DOCKER_CREDENTIALS_PSW" | docker login --username "$DOCKER_CREDENTIALS_USR" --password-stdin'
+        login(dockerHost) {
             body()
         }
     }
