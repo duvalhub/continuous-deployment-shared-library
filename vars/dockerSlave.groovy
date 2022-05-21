8e17aa71e57f91a51df611956f8bdf08a64e4473
Maxthod <maxthod@pm.me>
2022-05-21 13:01:25 -0400

    "improvement/slave-node-without-docker-slave"

--- vars/dockerSlave.groovy
+++ vars/dockerSlave.groovy
@@ -4,9 +4,9 @@ def call(Closure body) {
     if(!SharedLibrary.getVersion(env)) {
         initializeSharedLibrary.findVersion()
     }
-    String image = 'duvalhub/jenkins-slave:1.0.5.rc1'
-    dockerNode([image: image, args: '-v /var/jenkins:/home/jenkins']) {
+//    String image = 'duvalhub/jenkins-slave:1.0.5.rc1'
+//    dockerNode([image: image, args: '-v /var/jenkins:/home/jenkins']) {
       body()
-    }
+//    }
 }
 
