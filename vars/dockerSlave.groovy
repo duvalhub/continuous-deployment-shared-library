36d56c84aeb6b73189cfd25d4eb64b90b391e2fd
Maxthod <maxthod@pm.me>
2022-05-21 19:00:06 -0400

    Continue...

--- vars/dockerSlave.groovy
+++ vars/dockerSlave.groovy
@@ -9,6 +9,7 @@ def call(Closure body) {
 //    }
     String image = 'duvalhub/jenkins-slave:1.0.5.rc1'
     dockerNode([image: image, args: '-v /var/jenkins:/home/jenkins']) {
+        body()
     }
 }
 
