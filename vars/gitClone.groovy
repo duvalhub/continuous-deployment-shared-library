d894b61d3211c6ca8ec5a0e39419053268427f6d
Maxthod <maxthod@pm.me>
2022-06-21 00:59:55 -0400

    Continue...

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -5,10 +5,11 @@ import com.duvalhub.initializeworkdir.SharedLibrary
 def call(GitCloneRequest request) {
     echo "#### GitCloning with GitCloneRequest '${request.toString()}'"
     GitRepo gitRepo = request.gitRepo
-    withSshKey("github.com", "SERVICE_ACCOUNT_SSH", "git") {
+    String host = "github.com"
+    withSshKey(host, "SERVICE_ACCOUNT_SSH", "git") {
         withEnv([
                 "GIT_DIRECTORY=${request.directory}",
-                "GIT_URL=${gitRepo.getUrl().replace("github.com", env.SSH_HOST)}"
+                "GIT_URL=${gitRepo.getUrl().replace(host, env.SSH_HOST)}" // Since withSshKey create a random entry named '$SSH_HOST' in ssh config file
         ]) {
             String script = "${SharedLibrary.getWorkdir(env)}/libs/scripts/git/gitclone.sh"
             executeScript(script)
