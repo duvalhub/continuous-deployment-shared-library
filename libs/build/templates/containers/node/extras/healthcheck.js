a95dbc2b577bc2d04cebd9a58d1eff2803df2005
Hugues McNeil-Duval <hugues.mcneil-duval@duvalhub.com>
2022-12-11 22:44:06 -0500

    Continue...

--- libs/build/templates/containers/node/extras/healthcheck.js
+++ libs/build/templates/containers/node/extras/healthcheck.js
@@ -7,9 +7,11 @@ http.request({
     port: Number(process.env.PORT) || 80,
     path: process.env.HEALTHCHECK_ENDPOINT || '/api/healthcheck'
 }, function (response) {
-    response.on('data', () => { })
+    String str = ''
+    response.on('data', (chunk) => { str += chunk})
     response.on('end', function () {
         const statusCode = response.statusCode
+        console.log(str)
         if (statusCode >= 400) {
             process.exit(1)
         }
